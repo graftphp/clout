bbdc458b5c7c3e3d3b7e5f3c923dd6ae1dd08da5
Roger Hughes <roger@clicked.me.uk>
2017-06-20 21:34:46 +0100

    uikit3 login view

--- src/Views/login.php
+++ src/Views/login.php
@@ -5,35 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Clout Login</title>
-
     <link href="<?= \GraftPHP\Clout\Settings::cloutURL()?>/_/style.css" rel="stylesheet">
-
 </head>
 
 <body class="uk-height-1-1">
 
-    <div class="uk-vertical-align uk-text-center uk-height-1-1">
-        <div class="uk-vertical-align-middle" style="width: 250px;">
-
-            <form method="post" action="<?= \GraftPHP\Clout\Settings::cloutURL()?>/login" class="uk-panel uk-panel-box uk-form">
-                <h1>Clout</h1>
-                <div class="uk-form-row">
-                    <input class="uk-width-1-1 uk-form-large" type="text" name="username" placeholder="Username" value="<?=isset($username)?$username:''?>" required>
-                </div>
-                <div class="uk-form-row">
-                    <input class="uk-width-1-1 uk-form-large" type="password" name="password" placeholder="Password">
-                </div>
-                <div class="uk-form-row">
-                    <button type="submit" class="uk-width-1-1 uk-button uk-button-primary uk-button-large">Login</button>
-                </div>
-            </form>
-
-        </div>
+    <div class="uk-width-1-3 uk-position-center">
+        <form method="post" action="<?= \GraftPHP\Clout\Settings::cloutURL()?>/login" class="uk-panel uk-panel-box uk-form">
+            <h1>Clout</h1>
+            <div class="uk-form-row">
+                <input class="uk-input" type="text" name="username" placeholder="Username" value="<?=isset($username)?$username:''?>" required>
+            </div>
+            <div class="uk-form-row">
+                <input class="uk-input" type="password" name="password" placeholder="Password">
+            </div>
+            <div class="uk-form-row">
+                <button type="submit" class="uk-width-1-1 uk-button uk-button-primary">Login</button>
+            </div>
+        </form>
     </div>
 
-</body>
-
-<script src="<?= \GraftPHP\Clout\Settings::cloutURL()?>/_/script.js"></script>
-
+    <script src="<?= \GraftPHP\Clout\Settings::cloutURL()?>/_/script.js"></script>
 </body>
 </html>
