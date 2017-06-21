1ebf95df178cff4a5ab8ebb254ab80fac360d2c1
Roger Hughes <roger@clicked.me.uk>
2017-06-21 20:45:47 +0100

    more login view

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" class="uk-height-1-1">
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,24 +7,21 @@
     <title>Clout Login</title>
     <link href="<?= \GraftPHP\Clout\Settings::cloutURL()?>/_/style.css" rel="stylesheet">
 </head>
-
-<body class="uk-height-1-1">
-
+<body>
     <div class="uk-width-1-3 uk-position-center">
         <form method="post" action="<?= \GraftPHP\Clout\Settings::cloutURL()?>/login" class="uk-panel uk-panel-box uk-form">
             <h1>Clout</h1>
-            <div class="uk-form-row">
+            <div class="uk-margin">
                 <input class="uk-input" type="text" name="username" placeholder="Username" value="<?=isset($username)?$username:''?>" required>
             </div>
-            <div class="uk-form-row">
+            <div class="uk-margin">
                 <input class="uk-input" type="password" name="password" placeholder="Password">
             </div>
-            <div class="uk-form-row">
+            <div class="uk-margin">
                 <button type="submit" class="uk-width-1-1 uk-button uk-button-primary">Login</button>
             </div>
         </form>
     </div>
-
     <script src="<?= \GraftPHP\Clout\Settings::cloutURL()?>/_/script.js"></script>
 </body>
 </html>
