caeafa9752860f19a2efa1e3e852e08255024dbd
Roger Hughes <roger@clicked.me.uk>
2018-03-22 20:53:32 +0000

    return default settings if helper is called before db tables are set up, fill in missing settings and return default if they are missing from the table

--- src/Util/helpers.php
+++ src/Util/helpers.php
@@ -7,17 +7,46 @@ $clout_settings_array = [];
 
 function clout_settings($setting)
 {
-	if (!isset($GLOBALS['clout_settings_array'][$setting])) {
-		$db = new DB();
-        $res = $db->table('clout_settings')
-            ->where('label', '=', $setting)
-            ->get();
+    if (DB::tableExists('clout_settings')) {
+        if (!isset($GLOBALS['clout_settings_array'][$setting])) {
+            $db = new DB();
+            $res = $db->table('clout_settings')
+                ->where('label', '=', $setting)
+                ->get();
 
-		if (!isset($res->first()->value)) {
-			dd('setting not found "' . $setting . '"');
-		} else {
-			$GLOBALS['clout_settings_array'][$setting] = $res->first()->value;
-		}
-	}
-	return $GLOBALS['clout_settings_array'][$setting];
+            if (!isset($res->first()->value)) {
+                try {
+                    $value = array_filter(
+                        Settings::$db_defaultdata,
+                        function ($k) use ($setting) {
+                            return $k[1] == $setting;
+                        }
+                    )[0][2];
+                } catch (Exception $e) {
+                    dd('Default Setting ' . $setting . ' not found');
+                }
+                DB::insert(
+                    'clout_settings',
+                    ['label', 'value'],
+                    ['label' => $setting, 'value' => $value]
+                );
+                $GLOBALS['clout_settings_array'][$setting] = $value;
+            } else {
+                $GLOBALS['clout_settings_array'][$setting] = $res->first()->value;
+            }
+        }
+        return $GLOBALS['clout_settings_array'][$setting];
+    } else {
+        try {
+            $value = array_filter(
+                Settings::$db_defaultdata,
+                function ($k) use ($setting) {
+                    return $k[1] == $setting;
+                }
+            )[0][2];
+        } catch (Exception $e) {
+            dd('Default Setting ' . $setting . ' not found');
+        }
+        return $value;
+    }
 }
